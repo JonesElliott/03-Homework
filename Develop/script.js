040c33748551fb23aaeabde43ac62b7d59e04051
Elliott Jones <jones.elliott@outlook.com>
2020-07-15 11:52:36 -0700

    Added generatePassword function, added specialCharacters variable

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var specialCharacters = ['!', '@', '#', '$', '%', '&', '*', '(', ')', '+', '-', '/', ':', ';', '<', '=', '>', '?', '[', '/', ']', '^', '_', '`', '{', '}', '|', '~'];
+
+
+function generatePassword() {
+
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
