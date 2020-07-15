3f744973fc0de148535c3f1b56435b5c15e91ca1
Elliott Jones <jones.elliott@outlook.com>
2020-07-15 13:38:58 -0700

    Added a do while statement to catch if no password paramaters were entered

--- Develop/script.js
+++ Develop/script.js
@@ -2,17 +2,19 @@
 var generateBtn = document.querySelector("#generate");
 
 // Declaring Variables
-// Special characters
+// Password characters, numbers, & symbols
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "0123456789"
 var specialCharacters = "!#$%&()*+,-./:;<=>?@[]^_{|}~";
+
 // Password Options
 var pLength;
+var pUpCase = false;
+var pLowCase = false;
 var pNumbers = false;
 var pSpecialChars = false;
-var pUpCase = false;
-var pLowCase = false
+
 
 // Prompt user for password length
 do {
@@ -21,22 +23,35 @@ do {
 // Only allows password length within range
 while (pLength < 8 || pLength > 128);
 console.log(pLength); // Sanity Check
-// Will the password include upper case?
-pUpCase = confirm("Do you want upper case characters in the password?");
-console.log(pUpCase);
-// Will the password include lower case>
-pLowCase = confirm("Do you want lower case characters in the password?");
-console.log(pLowCase); // Sanity Check
-// Will the password include numbers?
-pNumbers = confirm("Do you want numbers in the password?");
-console.log(pNumbers); // Sanity Check
-// Will the password include special characters?
-pSpecialChars = confirm("Do you want special characters in the password?");
-console.log(pSpecialChars);
+
+do {
+  // Will the password include upper case?
+  pUpCase = confirm("Do you want upper case characters in the password?");
+  console.log(pUpCase); // Sanity Check
+
+  // Will the password include lower case>
+  pLowCase = confirm("Do you want lower case characters in the password?");
+  console.log(pLowCase); // Sanity Check
+
+  // Will the password include numbers?
+  pNumbers = confirm("Do you want numbers in the password?");
+  console.log(pNumbers); // Sanity Check
+
+  // Will the password include special characters?
+  pSpecialChars = confirm("Do you want special characters in the password?");
+  console.log(pSpecialChars);
+
+  if (!pUpCase && !pLowCase && !pNumbers && !pSpecialChars) {
+    alert("Please select password paramaters to continue");
+  }
+}
+
+while (!pUpCase && !pLowCase && !pNumbers && !pSpecialChars);
 
 // Function to generate password given user input
 function generatePassword() {
   var allowed = "";
+  // Add allowed paramaters to password generator
   if (pUpCase) {
     allowed = allowed + upperCase;
   }
@@ -52,15 +67,17 @@ function generatePassword() {
   console.log(allowed); // Sanity Check
 
   var password = "";
+  // Convert user given password length to an integer
   var length = parseInt(pLength);
   
+  // Loop to pick random allowed chars, nums, & Symbols at given password length
   for (i = 0; i < length; i++) {
     var random = Math.floor(Math.random() * allowed.length);
     password += allowed[random];
   }
+
+  // Return the generated password
   return password;
-
-
 }
 
 
