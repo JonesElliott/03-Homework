9464901b6faf9e891984e241c8f79a1e115e2379
Elliott Jones <jones.elliott@outlook.com>
2020-07-15 14:09:15 -0700

    Added button to choose new parameters

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var parametersBtn = document.querySelector("#parameters");
+
 // Declaring Variables
 // Password characters, numbers, & symbols
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -15,48 +17,50 @@ var pLowCase = false;
 var pNumbers = false;
 var pSpecialChars = false;
 
-
-// Prompt user for password length
-do {
-  pLength = prompt("Enter desired password length (Min: 8, Max: 128)");
-  // Alert message when given length is out of range
-  if (pLength < 8 || pLength > 128) {
-    alert("Please eneter a password length within range.")
+function getParameters() {
+  // Prompt user for password length
+  do {
+    pLength = prompt("Enter desired password length (Min: 8, Max: 128)");
+    // Alert message when given length is out of range
+    if (pLength < 8 || pLength > 128) {
+      alert("Please eneter a password length within range.")
+    }
   }
-}
-// Repeat above if given password length is out of range
-while (pLength < 8 || pLength > 128);
-console.log("Password Length: " + pLength); // Sanity Check
+  // Repeat above if given password length is out of range
+  while (pLength < 8 || pLength > 128);
+  console.log("Password Length: " + pLength); // Sanity Check
 
-// Get desired parameters
-do {
-  // Will the password include upper case?
-  pUpCase = confirm("Do you want upper case characters in the password?");
-  console.log("Upper Case: " + pUpCase); // Sanity Check
+  // Get desired parameters
+  do {
+    // Will the password include upper case?
+    pUpCase = confirm("Do you want upper case characters in the password?");
+    console.log("Upper Case: " + pUpCase); // Sanity Check
 
-  // Will the password include lower case>
-  pLowCase = confirm("Do you want lower case characters in the password?");
-  console.log("Lower Case:" + pLowCase); // Sanity Check
+    // Will the password include lower case>
+    pLowCase = confirm("Do you want lower case characters in the password?");
+    console.log("Lower Case:" + pLowCase); // Sanity Check
 
-  // Will the password include numbers?
-  pNumbers = confirm("Do you want numbers in the password?");
-  console.log("Numbers: " + pNumbers); // Sanity Check
+    // Will the password include numbers?
+    pNumbers = confirm("Do you want numbers in the password?");
+    console.log("Numbers: " + pNumbers); // Sanity Check
 
-  // Will the password include special characters?
-  pSpecialChars = confirm("Do you want special characters in the password?");
-  console.log("Special Characters: " + pSpecialChars);
+    // Will the password include special characters?
+    pSpecialChars = confirm("Do you want special characters in the password?");
+    console.log("Special Characters: " + pSpecialChars);
 
-  if (!pUpCase && !pLowCase && !pNumbers && !pSpecialChars) {
-    alert("Please select password paramaters to continue");
+    if (!pUpCase && !pLowCase && !pNumbers && !pSpecialChars) {
+      alert("Please select password paramaters to continue");
+    }
   }
+
+  // Repeat above if no parameters were selected
+  while (!pUpCase && !pLowCase && !pNumbers && !pSpecialChars);
 }
 
-while (!pUpCase && !pLowCase && !pNumbers && !pSpecialChars);
-
-// Function to generate password given user input
+// Function to generate password given user parameters
 function generatePassword() {
   var allowed = "";
-  
+
   // Add allowed paramaters to password generator
   if (pUpCase) {
     allowed = allowed + upperCase;
@@ -98,3 +102,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+parametersBtn.addEventListener("click", getParameters);
