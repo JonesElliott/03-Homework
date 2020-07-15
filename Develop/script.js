618dd3e8473cb70773c2d859ef587a0ed2e75239
Elliott Jones <jones.elliott@outlook.com>
2020-07-15 12:56:08 -0700

    Added confirms giving user option for upper and lower case

--- Develop/script.js
+++ Develop/script.js
@@ -8,6 +8,8 @@ var specialCharacters = ['!', '@', '#', '$', '%', '&', '*', '(', ')', '+', '-', 
 var pLength;
 var pNumbers = false;
 var pSpecialChars = false;
+var pUpCase = false;
+var pLowCase = false
 
 // Prompt user for password length
 do {
@@ -16,15 +18,22 @@ do {
 // Only allows password length within range
 while (pLength < 8 || pLength > 128);
 console.log(pLength); // Sanity Check
-
+// Will the password include upper case?
+pUpCase = confirm("Do you want upper case characters in the password?");
+console.log(pUpCase);
+// Will the password include lower case>
+pLowCase = confirm("Do you want lower case characters in the password?");
+console.log(pLowCase); // Sanity Check
+// Will the password include numbers?
 pNumbers = confirm("Do you want numbers in the password?");
-console.log(pNumbers);
+console.log(pNumbers); // Sanity Check
+// Will the password include special characters?
 pSpecialChars = confirm("Do you want special characters in the password?");
 console.log(pSpecialChars);
 
-
+// Function to generate password given user input
 function generatePassword() {
-
+  
 }
 
 
